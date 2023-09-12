73eed751b8fe1ce387072ec3a51bdab94fdba2f1
cowerino <nieve.rob@gmail.com>
2023-09-11 23:33:30 -0300

    merge from nipi

---
+++ uct_app/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'views/login.dart';
+import 'views/profile_grid.dart';
+import 'views/dashboard.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: LoginPage(),
+      routes: {
+        '/login': (context) => LoginPage(),
+        '/profile_grid': (context) => const ProfilesGrid(),
+        '/dashboard': (context) => const MyHomePage(title: 'DTE',),
+      },
+    );
+  }
+}
